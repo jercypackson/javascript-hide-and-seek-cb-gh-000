e73c8a8e4083acb7238d69fb2a1c8c56554f71a4
Laura Luidolt <laura.luidolt@gmail.com>
2017-12-25 19:08:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function getFirstSelector(selector){
   return document.querySelector(selector);
 }
+
+function nestedTarget(){
+  var nested = document.getElementById('nested');
+  return nested.querySelector('target');
+}
